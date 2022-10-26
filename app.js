15d6ab4cae4f6009305c4f6719ca114f95e3b315
skvarshasivakumar <skvarshasivakumar@gmail.com>
2022-10-26 12:01:08 +0530

    finally done

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const ejs = require("ejs");
 const mongoose=require("mongoose");
 var _=require("lodash");
 const truncate = (str, max, suffix) => str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
-const homeStartingContent ="Make sure your question is on-topic and valid. Doubt's Overflow only accepts certain types of questions about programming and software development,career related queries and your question must be  written in English. If your question is not on-topic or is otherwise unsuitable for this site,then it will likely be closed. 1.How to write a query? write your name followed by - then your Title then your query 2. How to answer a query? tag the person with @XXX in the title section - followed by your name then your answer";
+const homeStartingContent ="Make sure your question is on-topic and valid."
+const line2="Doubt's Overflow only accepts certain types of questions about programming and software development,career related queries and your question must be  written in English.";
+const line3 = "If your question is not on-topic or is otherwise unsuitable for this site,then it will likely be closed.";
+const line4= "How to write a query? write your name followed by - then your Title then your query";
+const line5="How to answer a query? tag the person with @XXX in the title section - followed by your name then your answer";
 const aboutContent = "A community-based space to find and contribute answers to technical doubts,career related queries and more importantly DOUBT'S OVERFLOW will be a mockup of one of the most popular websites in the world 'STACK OVERFLOW'. Stack Overflow is a question and answer website more useful for professional and enthusiast programmers." ;
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -26,7 +30,7 @@ app.get("/",function(req,res){
       console.log(err);
     }
     else{
-        res.render('home',{homeContent:homeStartingContent,postsnew:posts});
+        res.render('home',{homeContent:homeStartingContent,one:line2,two:line3,three:line3,four:line4,five:line5,postsnew:posts});
     }
   });
 });
